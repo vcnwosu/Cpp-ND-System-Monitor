2069dd7aaff18370fe8a3e2aa2028c4bd51a4fa9
vcnwosu <vcnwosu@gmail.com>
2019-11-13 16:05:57 -0600

    Uptime: Write uptime parsing and formating test

--- tests/linux_parser_test.cpp
+++ tests/linux_parser_test.cpp
@@ -3,19 +3,41 @@
 
 #include "system.h"
 #include "linux_parser.h"
+#include "format.h"
 
 static void memoryUtilization(System &system) {
     std::cout << "LinuxParser correctly parses memory utilization... ";
 
     const float memory = LinuxParser::MemoryUtilization();
 
-    assert(typeid(memory) == typeid(float));
     assert(memory > 0.0 && memory <= 100.0);
     assert(system.MemoryUtilization() == memory);
 
     std::cout << "PASS." << std::endl;
 }
 
+static void uptime(System &system) {
+    std::cout << "LinuxParser correctly parses uptime... ";
+
+    const long uptime = LinuxParser::UpTime();
+
+    assert(uptime > 0);
+    assert(system.UpTime() == uptime);
+
+    std::cout << "PASS." << std::endl;
+
+    std::cout << "Format helper function parses time correctly into HH:SS:MM... ";
+
+    assert(Format::ElapsedTime(0) == "00:00:00");
+    assert(Format::ElapsedTime(5) == "00:00:05");
+    assert(Format::ElapsedTime(3600) == "01:00:00");
+    assert(Format::ElapsedTime(1800) == "00:30:00");
+    assert(Format::ElapsedTime(127) == "00:02:07");
+
+    std::cout << "PASS." << std::endl;
+}
+
 void RunLinuxParserTest(System &system) {
     memoryUtilization(system);
+    uptime(system);
 }
